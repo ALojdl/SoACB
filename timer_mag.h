83d2cd17d45f54ca312c5f859d651ccafa77aaa3
niveskap <niveskaprocki@gmail.com>
2016-12-09 14:37:52 +0100

    Update timer_mag.h

--- timer_mag.h
+++ timer_mag.h
@@ -1,9 +1,13 @@
 #ifndef _TIMER_MAG_H
 #define _TIMER_MAG_H
 
+#define SEC_TO_MILI 1000
+#define MILI_TO_MICRO 1000
 
 double getMiliTimeDiff(struct timeval t_start, struct timeval t_end);
 
 struct timeval addTime(struct timeval t_time, int mili_time);
+
+int poason(double lambda);
 
 #endif
